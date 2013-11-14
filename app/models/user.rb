f1b375511716b3ac78e47ac3decea4d639021411
Dmitri Kiselevich <dmitri.web.master@gmail.com>
2013-11-14 13:16:48 +0200

    no nned to confirm users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :first_name, :last_name, :password, :password_confirmation, :email, :remember_mes
   validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
