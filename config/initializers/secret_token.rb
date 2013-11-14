f827297b79437646969e1007fe88de00616be3ed
Dmitri Kiselevich <dmitri.web.master@gmail.com>
2013-11-14 11:42:23 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Umsurvey::Application.config.secret_key_base = '0d9b8def92dadb5f037c3abc4bede1857dc87b42d339e8ab7c5fd23ce679b838ec5f0b43e7dbbc9c0d62f1f5815087e683ce0f02f2979142982931567975d304'
